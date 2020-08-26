fc54f387bcdb9e212db5b87a3f0d9ccbf932492f
Sheikh Sayed <sksayed@live.com>
2020-08-26 15:23:59 +0600

    initial commit

---
+++ app/src/main/java/com/sayed/sayedsimplealarm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sayed.sayedsimplealarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
